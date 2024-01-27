a3b6f0783019bbadae63031e5defcb585d73a89b
e1b21062 <e1b21062@st.oit.ac.jp>
2024-01-27 15:14:00 +0900

    リポジトリの初期化

---
+++ src/main/java/oit/is/z1976/fgo/fgo/FgoApplication.java
@@ -0,0 +1,13 @@
+package oit.is.z1976.fgo.fgo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FgoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FgoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/oit/is/z1976/fgo/fgo/FgoApplicationTests.java
@@ -0,0 +1,13 @@
+package oit.is.z1976.fgo.fgo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FgoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
